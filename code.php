ab0d06b0b0edd1a50ea4cd743149731fc4854c7b
kamalamirmahdi <160341532+kamalamirmahdi@users.noreply.github.com>
2024-07-24 19:57:59 +0330

    clear all the codes

--- code.php
+++ code.php
@@ -1,13 +1,3 @@
 <?php 
-    $cars = array("why"=>"bmw","pride","benz","phonix");
-    /*foreach($cars as $car){
-        echo $car . '<hr>';
-    }*/
-    /*for($i = 0 ; $i < 4 ; $i++){
-        echo $cars[$i] . '<br>';
-    }*/
-    //var_dump($cars);
-    //print_r($cars);
-    //echo $cars["why"];
-    //echo "I love you";
+    
 ?>
