9691de01e4bb403ca69003641cbb6242dc4aa6de
kamalamirmahdi <160341532+kamalamirmahdi@users.noreply.github.com>
2024-07-27 09:59:24 +0330

    Hi

--- code.php
+++ code.php
@@ -1,26 +1,3 @@
 <?php 
-    $users = array(
-        "user_1" => array(
-            "name" => "erfan",
-            "email" => "erfan@gmail.com",
-            "username" => "erfan704"
-        ),
-        "user_2" => array(
-            "name" => array(
-                "first_name" => "ali",
-                "last_name" => "moodi"
-            ),
-            "email" => "ali@gmail.com",
-            "username" => "ali110"
-        ),
-        "user_3" => array(
-            "name" => "maryam",
-            "email" => "maryam@gmail.com",
-            "username" => "maryam502"
-        )
-        );
-    //var_dump($users);
-    //print_r($users["user_1"]["name"]);
-    //echo "<br>";
-    //echo $users["user_2"]["name"]["last_name"];
+    echo "Hello World!";
 ?>
