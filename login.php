d792457baae473aeffacd76f96c06b0011eb1b64
kamalamirmahdi <160341532+kamalamirmahdi@users.noreply.github.com>
2024-07-28 08:27:23 +0330

    ali

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
     include("index.html");
     function username($username){
-        if(empty($username)){
-            echo "something is meissing";
+        if($username == "ali"){
+            echo true;
         }
     }
     function password($password){
