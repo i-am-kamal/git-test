a4b263dd52d028279e48d766e7d9ce8646674dc4
kamalamirmahdi <160341532+kamalamirmahdi@users.noreply.github.com>
2024-09-28 03:55:03 +0330

    start php again

--- code.php
+++ code.php
@@ -1,13 +1,3 @@
 <?php
-    $wh_questions = array(
-        "what",
-        "how",
-        "when",
-        "where",
-        "whose",
-        "why"
-    )
-    foreach($wh_questions as $wh_question){
-        echo $wh_question;
-    }
+    echo "salam";
 ?>
